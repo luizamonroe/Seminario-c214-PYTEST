a7b600083673c40de747dd73db7c9c7bf959ea10
Luiza <luizamonroe@gec.inatel.br>
2022-04-28 16:05:23 -0300

    Criação da calculadora

---
+++ Calculadora/calculadora.py
@@ -0,0 +1,16 @@
+class Calculadora:
+    def soma(x, y):
+        return x + y
+
+    def subtracao(x, y):
+        return x - y
+
+    def multiplicacao(x, y):
+        return x * y
+
+    def divisao(x, y):
+        if y == 0:
+            return 0
+        else:
+            return x / y
+
